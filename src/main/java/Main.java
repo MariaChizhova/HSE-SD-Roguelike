c10b84b7e6443f248892de4dd263a9246de09f7e
German Tarabonda <57793086+kot239@users.noreply.github.com>
2022-04-09 23:44:42 +0300

    Add main menu drawer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,9 @@
+import view.ConsoleDrawer;
+import view.MenuState;
+
 public class Main {
+    public static void main(String[] args) {
+        var consoleDrawer = new ConsoleDrawer();
+        consoleDrawer.drawMenu(MenuState.START);
+    }
 }
--- src/main/java/view/ConsoleDrawer.java
+++ src/main/java/view/ConsoleDrawer.java
@@ -17,10 +17,14 @@ public class ConsoleDrawer {
 
     DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
     Terminal terminal = null;
+    Screen screen = null;
 
     public ConsoleDrawer() {
         try {
             terminal = defaultTerminalFactory.createTerminal();
+            screen = new TerminalScreen(terminal);
+
+            screen.startScreen();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -34,84 +38,165 @@ public class ConsoleDrawer {
         
     }
 
+    private void addTextButton(TextGraphics textGraphics, String textLabel, int column, int row) {
+        TerminalPosition labelBoxTopLeft = new TerminalPosition(column, row);
+        TerminalSize labelBoxSize = new TerminalSize(textLabel.length() + 2, 3);
+        TerminalPosition labelBoxTopRightCorner = labelBoxTopLeft.withRelativeColumn(labelBoxSize.getColumns() - 1);
+
+        textGraphics.fillRectangle(labelBoxTopLeft, labelBoxSize, ' ');
+
+        /*
+        Draw horizontal lines, first upper then lower
+         */
+        textGraphics.drawLine(
+                labelBoxTopLeft.withRelativeColumn(1),
+                labelBoxTopLeft.withRelativeColumn(labelBoxSize.getColumns() - 2),
+                Symbols.DOUBLE_LINE_HORIZONTAL);
+        textGraphics.drawLine(
+                labelBoxTopLeft.withRelativeRow(2).withRelativeColumn(1),
+                labelBoxTopLeft.withRelativeRow(2).withRelativeColumn(labelBoxSize.getColumns() - 2),
+                Symbols.DOUBLE_LINE_HORIZONTAL);
+        /*
+        Manually do the edges and (since it's only one) the vertical lines, first on the left then on the right
+         */
+        textGraphics.setCharacter(labelBoxTopLeft, Symbols.DOUBLE_LINE_TOP_LEFT_CORNER);
+        textGraphics.setCharacter(labelBoxTopLeft.withRelativeRow(1), Symbols.DOUBLE_LINE_VERTICAL);
+        textGraphics.setCharacter(labelBoxTopLeft.withRelativeRow(2), Symbols.DOUBLE_LINE_BOTTOM_LEFT_CORNER);
+        textGraphics.setCharacter(labelBoxTopRightCorner, Symbols.DOUBLE_LINE_TOP_RIGHT_CORNER);
+        textGraphics.setCharacter(labelBoxTopRightCorner.withRelativeRow(1), Symbols.DOUBLE_LINE_VERTICAL);
+        textGraphics.setCharacter(labelBoxTopRightCorner.withRelativeRow(2), Symbols.DOUBLE_LINE_BOTTOM_RIGHT_CORNER);
+
+        /*
+        Finally put the text inside the box
+         */
+        textGraphics.putString(labelBoxTopLeft.withRelative(1, 1), textLabel);
+    }
+
+    private void drawCat(int column, int row) {
+        screen.setCharacter(column + 3, row, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        screen.setCharacter(column + 13, row, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        screen.setCharacter(column + 2, row + 1, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        screen.setCharacter(column + 2, row + 2, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        screen.setCharacter(column + 4, row + 1, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        screen.setCharacter(column + 5, row + 2, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        screen.setCharacter(column + 11, row + 2, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        screen.setCharacter(column + 12, row + 1, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        screen.setCharacter(column + 14, row + 1, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        screen.setCharacter(column + 14, row + 2, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        for (int i = 0; i < 5; i++) {
+            screen.setCharacter(column + 6 + i, row + 3, new TextCharacter(
+                    ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        }
+        for (int i = 0; i < 3; i++) {
+            screen.setCharacter(column + 1, row + 3 + i, new TextCharacter(
+                    ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+            screen.setCharacter(column + 15, row + 3 + i, new TextCharacter(
+                    ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        }
+        for (int i = 0; i < 7; i++) {
+            screen.setCharacter(column, row + 6 + i, new TextCharacter(
+                    ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+            screen.setCharacter(column + 16, row + 6 + i, new TextCharacter(
+                    ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        }
+        screen.setCharacter(column + 1, row + 13, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        screen.setCharacter(column + 15, row + 13, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        for (int i = 0; i < 2; i++) {
+            screen.setCharacter(column + 2 + i, row + 14, new TextCharacter(
+                    ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+            screen.setCharacter(column + 13 + i, row + 14, new TextCharacter(
+                    ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        }
+        for (int i = 0; i < 9; i++) {
+            screen.setCharacter(column + 4 + i, row + 15, new TextCharacter(
+                    ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        }
+        screen.setCharacter(column + 3, row + 8, new TextCharacter(
+                ' ', TextColor.ANSI.GREEN, TextColor.ANSI.GREEN));
+        screen.setCharacter(column + 5, row + 8, new TextCharacter(
+                ' ', TextColor.ANSI.GREEN, TextColor.ANSI.GREEN));
+        screen.setCharacter(column + 11, row + 8, new TextCharacter(
+                ' ', TextColor.ANSI.GREEN, TextColor.ANSI.GREEN));
+        screen.setCharacter(column + 13, row + 8, new TextCharacter(
+                ' ', TextColor.ANSI.GREEN, TextColor.ANSI.GREEN));
+        screen.setCharacter(column + 8, row + 10, new TextCharacter(
+                ' ', TextColor.ANSI.MAGENTA, TextColor.ANSI.MAGENTA));
+
+        screen.setCharacter(column + 2, row + 8, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        screen.setCharacter(column + 6, row + 8, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        screen.setCharacter(column + 10, row + 8, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        screen.setCharacter(column + 14, row + 8, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+
+        for (int i = 0; i < 3; i++) {
+            screen.setCharacter(column + 3 + i, row + 7, new TextCharacter(
+                    ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+            screen.setCharacter(column + 3 + i, row + 9, new TextCharacter(
+                    ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+            screen.setCharacter(column + 11 + i, row + 7, new TextCharacter(
+                    ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+            screen.setCharacter(column + 11 + i, row + 9, new TextCharacter(
+                    ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        }
+
+        screen.setCharacter(column + 6, row + 11, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        screen.setCharacter(column + 7, row + 12, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        screen.setCharacter(column + 8, row + 11, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        screen.setCharacter(column + 9, row + 12, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+        screen.setCharacter(column + 10, row + 11, new TextCharacter(
+                ' ', TextColor.ANSI.WHITE, TextColor.ANSI.WHITE));
+    }
+
     /**
      * Function that draws menu in the terminal
      */
     public void drawMenu(MenuState menuState) {
         try {
-            /*
-            terminal.enterPrivateMode();
-            terminal.clearScreen();
-            terminal.setCursorVisible(false);
-            for (int i = 0; i < 60; i++) {
-                terminal.putCharacter('#');
-            }
-            terminal.putCharacter('\n');
-            for (int j = 0; j < 18; j++) {
-                terminal.putCharacter('#');
-                terminal.putCharacter('#');
-                for (int i = 0; i < 56; i++) {
-                    terminal.putCharacter(' ');
-                }
-                terminal.putCharacter('#');
-                terminal.putCharacter('#');
-                terminal.putCharacter('\n');
-            }
-            for (int i = 0; i < 60; i++) {
-                terminal.putCharacter('#');
-            }
-            terminal.putCharacter('\n');
-
-            terminal.flush();
-
-             */
-
-            Screen screen = new TerminalScreen(terminal);
-
-            screen.startScreen();
             screen.setCursorPosition(null);
 
             TerminalSize terminalSize = screen.getTerminalSize();
+            for (int column = 0; column < terminalSize.getColumns(); column++) {
+                screen.setCharacter(column, 0, new TextCharacter(
+                        '#', TextColor.ANSI.DEFAULT, TextColor.ANSI.DEFAULT));
+                screen.setCharacter(column, terminalSize.getRows() - 1, new TextCharacter(
+                        '#', TextColor.ANSI.DEFAULT, TextColor.ANSI.DEFAULT));
+            }
+            for (int row = 0; row < terminalSize.getRows(); row++) {
+                screen.setCharacter(0, row, new TextCharacter(
+                        '#', TextColor.ANSI.DEFAULT, TextColor.ANSI.DEFAULT));
+                screen.setCharacter(terminalSize.getColumns() - 1, row, new TextCharacter(
+                        '#', TextColor.ANSI.DEFAULT, TextColor.ANSI.DEFAULT));
+            }
 
-
-            /*
-            Just like with Terminal, it's probably easier to draw using TextGraphics. Let's do that to put a little
-            box with information on the size of the terminal window
-             */
-            String sizeLabel = "Terminal Size: " + terminalSize;
-            TerminalPosition labelBoxTopLeft = new TerminalPosition(1, 1);
-            TerminalSize labelBoxSize = new TerminalSize(sizeLabel.length() + 2, 3);
-            TerminalPosition labelBoxTopRightCorner = labelBoxTopLeft.withRelativeColumn(labelBoxSize.getColumns() - 1);
             TextGraphics textGraphics = screen.newTextGraphics();
-            //This isn't really needed as we are overwriting everything below anyway, but just for demonstrative purpose
-            textGraphics.fillRectangle(labelBoxTopLeft, labelBoxSize, ' ');
+            String startLabel = "  Start game  ";
+            String loadLabel  = "  Load game   ";
+            String exitLabel  = "  Exit game   ";
+            addTextButton(textGraphics, startLabel, 7, 4);
+            addTextButton(textGraphics, loadLabel, 7, 10);
+            addTextButton(textGraphics, exitLabel, 7, 16);
 
-            /*
-            Draw horizontal lines, first upper then lower
-             */
-            textGraphics.drawLine(
-                    labelBoxTopLeft.withRelativeColumn(1),
-                    labelBoxTopLeft.withRelativeColumn(labelBoxSize.getColumns() - 2),
-                    Symbols.DOUBLE_LINE_HORIZONTAL);
-            textGraphics.drawLine(
-                    labelBoxTopLeft.withRelativeRow(2).withRelativeColumn(1),
-                    labelBoxTopLeft.withRelativeRow(2).withRelativeColumn(labelBoxSize.getColumns() - 2),
-                    Symbols.DOUBLE_LINE_HORIZONTAL);
-
-            /*
-            Manually do the edges and (since it's only one) the vertical lines, first on the left then on the right
-             */
-            textGraphics.setCharacter(labelBoxTopLeft, Symbols.DOUBLE_LINE_TOP_LEFT_CORNER);
-            textGraphics.setCharacter(labelBoxTopLeft.withRelativeRow(1), Symbols.DOUBLE_LINE_VERTICAL);
-            textGraphics.setCharacter(labelBoxTopLeft.withRelativeRow(2), Symbols.DOUBLE_LINE_BOTTOM_LEFT_CORNER);
-            textGraphics.setCharacter(labelBoxTopRightCorner, Symbols.DOUBLE_LINE_TOP_RIGHT_CORNER);
-            textGraphics.setCharacter(labelBoxTopRightCorner.withRelativeRow(1), Symbols.DOUBLE_LINE_VERTICAL);
-            textGraphics.setCharacter(labelBoxTopRightCorner.withRelativeRow(2), Symbols.DOUBLE_LINE_BOTTOM_RIGHT_CORNER);
-
-            /*
-            Finally put the text inside the box
-             */
-            textGraphics.putString(labelBoxTopLeft.withRelative(1, 1), sizeLabel);
+            drawCat(33, 4);
+            drawCat(53, 4);
 
             screen.refresh();
         } catch (Exception e) {
