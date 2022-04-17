e5e4e9bed1f6ec91c332014e597a7a64605b5722
German Tarabonda <57793086+kot239@users.noreply.github.com>
2022-04-18 00:49:28 +0300

    Add test

--- src/test/java/model/PlayerTest.java
+++ src/test/java/model/PlayerTest.java
@@ -2,9 +2,9 @@ package model;
 
 import model.inventory.Artifact;
 import model.inventory.ArtifactName;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import model.strategies.SimpleStrategy;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class PlayerTest {
 
@@ -103,5 +103,18 @@ public class PlayerTest {
         Assertions.assertEquals(1, player.getArmor() - oldArmor);
         Assertions.assertEquals(1, player.getDamage() - oldDamage);
         Assertions.assertEquals(100, player.getHealth());
+    }
+
+    @Test
+    public void addArtifactTest() {
+        var player = new Player(new Position(5, 5));
+        var oldArmor = player.getArmor();
+        var oldDamage = player.getDamage();
+        var woodenSword = new Artifact(ArtifactName.WOODEN_SWORD, 20, 0);
+        var helmet = new Artifact(ArtifactName.HELMET, 0, 30);
+        player.addArtifact(woodenSword);
+        player.addArtifact(helmet);
+        Assertions.assertEquals(30, player.getArmor() - oldArmor);
+        Assertions.assertEquals(20, player.getDamage() - oldDamage);
     }
 }
