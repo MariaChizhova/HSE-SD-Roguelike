8a8eb93af2507a293d6b942988b4962337be8577
Maria <mariachizhova17@gmail.com>
2022-04-17 19:22:06 +0300

    Comments to methods

--- src/main/java/model/decorator/ConfuseStrategyDecorator.java
+++ src/main/java/model/decorator/ConfuseStrategyDecorator.java
@@ -6,14 +6,28 @@ import model.strategies.StrategyEnemy;
 
 import java.io.Serializable;
 
+/**
+ * Confusing decorator of enemy strategy
+ */
 public class ConfuseStrategyDecorator extends StrategyDecorator implements Serializable {
     private int confusedNumber;
 
+    /**
+     * Creates ConfusedStrategyDecorator instance
+     * @param decorate strategy to decorate
+     * @param confusedNumber total number of confusing turns
+     */
     public ConfuseStrategyDecorator(StrategyEnemy decorate, int confusedNumber) {
         super(decorate);
         this.confusedNumber = confusedNumber;
     }
 
+    /**
+     *  Makes one enemy move
+     * @param playerPosition - player's position
+     * @param enemyPosition - old position
+     * @return new position
+     */
     @Override
     public Position nextMove(Position playerPosition, Position enemyPosition) {
         if (confusedNumber > 0) {
