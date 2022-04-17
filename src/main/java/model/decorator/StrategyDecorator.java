cc9f50540725835ace872ed4b96b466220f2ba63
Maria <mariachizhova17@gmail.com>
2022-04-17 19:22:06 +0300

    Comments to methods

--- src/main/java/model/decorator/StrategyDecorator.java
+++ src/main/java/model/decorator/StrategyDecorator.java
@@ -5,13 +5,26 @@ import model.strategies.StrategyEnemy;
 
 import java.io.Serializable;
 
+/**
+ * Decorator for enemy strategies
+ */
 public abstract class StrategyDecorator implements StrategyEnemy, Serializable {
     protected StrategyEnemy decorate;
 
+    /**
+     * Creates StrategyDecorator instance
+     * @param decorate
+     */
     public StrategyDecorator(StrategyEnemy decorate) {
         this.decorate = decorate;
     }
 
+    /**
+     * Makes one enemy move
+     * @param playerPosition - player's position
+     * @param enemyPosition - old position
+     * @return new position
+     */
     @Override
     public Position nextMove(Position playerPosition, Position enemyPosition) {
         return decorate.nextMove(playerPosition, enemyPosition);
