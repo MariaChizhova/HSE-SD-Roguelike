136cc50d645116de28b4fe8a539aa0f310f93067
German Tarabonda <57793086+kot239@users.noreply.github.com>
2022-06-25 15:39:28 +0300

    Fix tests

--- src/test/java/ru/hse/roguelike/model/StrategyTest.java
+++ src/test/java/ru/hse/roguelike/model/StrategyTest.java
@@ -6,11 +6,22 @@ import ru.hse.roguelike.model.strategies.SimpleStrategy;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Collections;
 
 import java.util.List;
 
 public class StrategyTest {
+
+    public List<Position> createEmptyPositions(Position enemyPosition) {
+        List<Position> positions = new ArrayList<>();
+        var xList = List.of(-1, 0, 1, 0);
+        var yList = List.of(0, -1, 0, 1);
+        for (int i = 0; i < xList.size(); i++) {
+            positions.add(new Position(enemyPosition.getX() + xList.get(i), enemyPosition.getY() + yList.get(i)));
+        }
+        return positions;
+    }
 
     @Test
     public void testSimpleStrategy() {
@@ -50,7 +61,7 @@ public class StrategyTest {
         var aggressiveStrategy = new AggressiveStrategy();
         var playerPosition = new Position(2, 2);
         var enemyPosition = new Position(6, 6);
-        Assertions.assertEquals(enemyPosition, aggressiveStrategy.nextMove(playerPosition, enemyPosition, 1, List.of()));
+        Assertions.assertEquals(enemyPosition, aggressiveStrategy.nextMove(playerPosition, enemyPosition, 1, createEmptyPositions(enemyPosition)));
     }
 
     @Test
@@ -66,7 +77,8 @@ public class StrategyTest {
             var playerPosition = new Position(playerXPoses.get(i), playerYPoses.get(i));
             var enemyPosition = new Position(enemyXPoses.get(i), enemyYPoses.get(i));
             var newEnemyPosition = new Position(resXPoses.get(i), resYPoses.get(i));
-            Assertions.assertEquals(newEnemyPosition, aggressiveStrategy.nextMove(playerPosition, enemyPosition, 5, List.of()));
+            var result = aggressiveStrategy.nextMove(playerPosition, enemyPosition, 5, createEmptyPositions(enemyPosition));
+            Assertions.assertEquals(newEnemyPosition, result);
         }
     }
 }
